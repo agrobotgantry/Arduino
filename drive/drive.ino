64767725d34f96032e939aa24c944169e1f53604
CasDam <c.damen@student.avans.nl>
2023-11-03 05:15:30 -0700

    Aansturing wielen eerste versie
    Eerste versie van aansturing wielen waarbij de ultrasone sensoren publishen in topics (getest) en de motoren gesubscribed zijn op topics (niet getest)

--- drive/drive.ino
+++ drive/drive.ino
@@ -7,14 +7,7 @@
 // Include ROS serial to the code
 #include <ros.h>
 #include <std_msgs/Float32.h>
-
-// Create ROS node handle and include messages
-ros::NodeHandle  nh;
-std_msgs::Float32 float32_msg;
-
-// Create ROS publishers
-ros::Publisher US1_publisher("agrobot_drive/ultrasonic_1", &float32_msg);
-ros::Publisher US2_publisher("agrobot_drive/ultrasonic_2", &float32_msg);
+#include <std_msgs/Int8.h>
 
 // Pins ultrasonic sensor 1 (Front left)
 const int US1_trigPin = 30;
@@ -57,15 +50,38 @@ const int M4_pulsPin = 12;
 const int M4_dirPin = 28;
 const int M4_enPin = 29;
 
+// Create ROS node handle and include messages
+ros::NodeHandle  nh;
+std_msgs::Float32 float32_msg;
+std_msgs::Int8 int8_msg;
+
+// Create ROS publishers
+ros::Publisher US1_publisher("agrobot_drive/ultrasonic_1", &float32_msg);
+ros::Publisher US2_publisher("agrobot_drive/ultrasonic_2", &float32_msg);
+ros::Publisher US3_publisher("agrobot_drive/ultrasonic_3", &float32_msg);
+ros::Publisher US4_publisher("agrobot_drive/ultrasonic_4", &float32_msg);
+
+// Create ROS subsribers
+ros::Subscriber<std_msgs::Int8> M1_subscriber("agrobot_drive/motor_1", &motor_1_callback);
+ros::Subscriber<std_msgs::Int8> M2_subscriber("agrobot_drive/motor_2", &motor_2_callback);
+ros::Subscriber<std_msgs::Int8> M3_subscriber("agrobot_drive/motor_3", &motor_3_callback);
+ros::Subscriber<std_msgs::Int8> M4_subscriber("agrobot_drive/motor_4", &motor_4_callback);
+
 
 void setup() {
   // Set baudraute for serial
   Serial.begin(57600);
 
-  // Initialise ROS node and advertise publishers
+  // Initialise ROS node, advertise publishers and subscribe to topics
   nh.initNode();
   nh.advertise(US1_publisher);
   nh.advertise(US2_publisher);
+  nh.advertise(US3_publisher);
+  nh.advertise(US4_publisher);
+  nh.subscribe(M1_subscriber);
+  nh.subscribe(M2_subscriber);
+  nh.subscribe(M3_subscriber);
+  nh.subscribe(M4_subscriber);
 
   // Initialise ultrasonic sensor 1 (Front left)
   pinMode(US1_echoPin, INPUT);
@@ -111,31 +127,102 @@ void setup() {
 
 void loop() {
   // Get the distances of the ultrasonic sensors
-  float US1_distance = ultrasonic_sensor(US1_trigPin,US1_echoPin);
-  float US2_distance = ultrasonic_sensor(US2_trigPin,US2_echoPin);
-  //float US3_distance = ultrasonic_sensor(US3_trigPin,US3_echoPin);
-  //float US4_distance = ultrasonic_sensor(US4_trigPin,US4_echoPin);
+  float US1_distance = ultrasonic_sensor_distance(US1_trigPin,US1_echoPin);
+  float US2_distance = ultrasonic_sensor_distance(US2_trigPin,US2_echoPin);
+  float US3_distance = ultrasonic_sensor_distance(US3_trigPin,US3_echoPin);
+  float US4_distance = ultrasonic_sensor_distance(US4_trigPin,US4_echoPin);
 
   // Publish the data of the ultrasonic sensor
-  float32_msg.data = US1_distance;
-  US1_publisher.publish(&float32_msg);
-
-  float32_msg.data = US2_distance;
-  US2_publisher.publish(&float32_msg);
+  publish_ultrasonic_sensor(US1_publisher, US1_distance);
+  publish_ultrasonic_sensor(US2_publisher, US2_distance);
+  publish_ultrasonic_sensor(US3_publisher, US3_distance);
+  publish_ultrasonic_sensor(US4_publisher, US4_distance);
   
-  nh.spinOnce();
-
-  // Print the distance of the ultrasonic sensor
-  Serial.print("Distance sensor 1: ");
-  Serial.println(US1_distance);
-  Serial.print("Distance sensor 2: ");
-  Serial.println(US2_distance);
   delay(100);
+  nh.spinOnce();
+}
+
+
+// Function to turn the stepper motor on. The direction should be positive (HIGH = 1) or negative (LOW = 0)
+void motor_on(int motor_number, int motor_direction){
+  // Select the correct motor to turn on
+  if(motor_number == 1) {
+    // Motor front left
+    digitalWrite(M1_enPin, LOW);
+    digitalWrite(M1_dirPin, motor_direction);
+    analogWrite(M1_pulsPin, 128);
+    Serial.println("Motor 1 turned on");
+  } else if(motor_number == 2) {
+    // Motor front right
+    digitalWrite(M2_enPin, LOW);
+    digitalWrite(M2_dirPin, motor_direction);
+    analogWrite(M2_pulsPin, 128);
+    Serial.println("Motor 2 turned on");
+  } else if(motor_number == 3) {
+    // Motor back right
+    digitalWrite(M3_enPin, LOW);
+    digitalWrite(M3_dirPin, motor_direction);
+    analogWrite(M3_pulsPin, 128);
+    Serial.println("Motor 3 turned on");
+  } else if(motor_number == 4) {
+    // Motor back left
+    digitalWrite(M4_enPin, LOW);
+    digitalWrite(M4_dirPin, motor_direction);
+    analogWrite(M4_pulsPin, 128);
+    Serial.println("Motor 4 turned on");
+  }
+}
+
+
+// Function to turn the stepper motor off
+void motor_off(int motor_number){
+  // Select the correct motor to turn off
+  if(motor_number == 1) {
+    // Motor front left
+    digitalWrite(M1_enPin, LOW);
+    digitalWrite(M1_dirPin, LOW);
+    analogWrite(M1_pulsPin, 0);
+    Serial.println("Motor 1 turned off");
+  } else if(motor_number == 2) {
+    // Motor front right
+    digitalWrite(M2_enPin, LOW);
+    digitalWrite(M2_dirPin, LOW);
+    analogWrite(M2_pulsPin, 0);
+    Serial.println("Motor 2 turned off");
+  } else if(motor_number == 3) {
+    // Motor back right
+    digitalWrite(M3_enPin, LOW);
+    digitalWrite(M3_dirPin, LOW);
+    analogWrite(M3_pulsPin, 0);
+    Serial.println("Motor 3 turned off");
+  } else if(motor_number == 4) {
+    // Motor back left
+    digitalWrite(M4_enPin, LOW);
+    digitalWrite(M4_dirPin, LOW);
+    analogWrite(M4_pulsPin, 0);
+    Serial.println("Motor 4 turned off");
+  }
+}
+
+
+// Function to control the motor after a callback from the subscriber
+void motor_control(int motor_number, int motor_state) {
+  // Select the correct action for the stepper motor
+  if(motor_state == 0) {
+    // Turn motor off
+    motor_off(motor_number);
+  } else if(motor_state == 1) {
+    // Turn motor on in positive direction
+    motor_on(motor_number,1);
+  } else if(motor_state == 2) {
+    // Turn motor on in negative direction
+    motor_on(motor_number,0);
+  }
 }
 
 
 // Function to retrieve the distance the ultrasonic sensor measures
-float ultrasonic_sensor(int triggerPin,int echoPin)
+float ultrasonic_sensor_distance(int triggerPin,int echoPin)
 {
   // Initialise variable
   float duration, distance;
@@ -153,4 +240,39 @@ float ultrasonic_sensor(int triggerPin,int echoPin)
   delay(100);
    
   return (distance);
+}
+
+
+// Funtion to publish the value of a ultrasonic sensor to ROS
+void publish_ultrasonic_sensor(ros::Publisher publisher, float distance) {
+  float32_msg.data = distance;
+  publisher.publish(&float32_msg);
+}
+
+
+// Subscriber callback stepper motor 1
+void motor_1_callback(const std_msgs::Int8 &message) {
+  // Control the motor
+  motor_control(1,message.data);
+}
+
+
+// Subscriber callback stepper motor 2
+void motor_2_callback(const std_msgs::Int8 &message) {
+  // Control the motor
+  motor_control(2,message.data);
+}
+
+
+// Subscriber callback stepper motor 3
+void motor_3_callback(const std_msgs::Int8 &message) {
+  // Control the motor
+  motor_control(3,message.data);
+}
+
+
+// Subscriber callback stepper motor 4
+void motor_4_callback(const std_msgs::Int8 &message) {
+  // Control the motor
+  motor_control(4,message.data);
 }
